795117a5cf3245436d8d2b37dc790f4eba9401a1
sango <aitaiyosora300@gmail.com>
2025-05-10 10:57:06 +0900

    refactor systemtray

--- app/host/systemtray.py
+++ app/host/systemtray.py
@@ -1,8 +1,9 @@
 import threading
+from logging import getLogger
+
 import pystray
 from PIL import Image
 from app.host.notifer import notify
-from logging import getLogger
 
 logger = getLogger(__name__)
 
@@ -10,42 +11,41 @@ tray_icon = None
 connection_status = "未接続"
 register_with_retry = None  # 外部からセットされる
 
+
 def setup_tray():
     global tray_icon
+    tray_icon = pystray.Icon("RemotePhone", Image.open("app.ico"), menu=_generate_menu())
+    return tray_icon
 
-    def generate_menu():
-        return pystray.Menu(
-            pystray.MenuItem(f"サーバー接続状態：{connection_status}", None, enabled=False),
-            pystray.MenuItem("サーバーに再接続", on_reconnect),
-            pystray.MenuItem("スマホで接続", lambda icon, item: notify("スマホで接続するには、QRコードをスキャンしてください。")),
-            pystray.MenuItem("終了", on_quit)
-        )
-
-    icon = pystray.Icon("RemotePhone", Image.open("app.ico"), menu=generate_menu())
-    tray_icon = icon
-    return icon
 
 def update_tray():
     if tray_icon:
-        tray_icon.menu = pystray.Menu(
-            pystray.MenuItem(f"接続状態：{connection_status}", None, enabled=False),
-            pystray.MenuItem("サーバーに再接続", on_reconnect),
-            pystray.MenuItem("スマホで接続", lambda icon, item: notify("スマホで接続するには、QRコードをスキャンしてください。")),
-            pystray.MenuItem("終了", on_quit)
-        )
+        tray_icon.menu = _generate_menu()
         tray_icon.update_menu()
 
-def on_reconnect(icon, item):
+
+def _generate_menu():
+    return pystray.Menu(
+        pystray.MenuItem(f"サーバー接続状態：{connection_status}", None, enabled=False),
+        pystray.MenuItem("サーバーに再接続", _on_reconnect),
+        pystray.MenuItem("スマホで接続", lambda icon, item: notify("スマホで接続するには、QRコードをスキャンしてください。")),
+        pystray.MenuItem("終了", _on_quit)
+    )
+
+
+def _on_reconnect(icon, item):
     logger.info("再接続がリクエストされました。")
     notify("サーバーへの再接続を試みます。")
 
-    def reconnect():
-        if register_with_retry:
-            with threading.Lock():
-                register_with_retry()
+    threading.Thread(target=_do_reconnect, daemon=True).start()
 
-    threading.Thread(target=reconnect, daemon=True).start()
 
-def on_quit(icon, item):
+def _do_reconnect():
+    if register_with_retry:
+        with threading.Lock():
+            register_with_retry()
+
+
+def _on_quit(icon, item):
     logger.info("終了処理が呼ばれました。")
     icon.stop()
