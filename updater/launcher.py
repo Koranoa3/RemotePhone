291c0fa711362875ab15290532c6c1c5774d6626
Koranoa3 <aitaiyosora300@gmail.com>
2025-06-26 15:55:27 +0900

    🗑️古いバージョンのソースを削除

--- updater/launcher.py
+++
@@ -1,7 +0,0 @@
-import subprocess
-
-def launch_new_app(app_path):
-    try:
-        subprocess.Popen([app_path], shell=False)
-    except Exception as e:
-        print(f"[Error] Failed to launch the new app: {e}")
