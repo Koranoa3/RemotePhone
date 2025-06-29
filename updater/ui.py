7c08ed2093876d0cf9391944209e65039e4584f0
Koranoa3 <aitaiyosora300@gmail.com>
2025-06-29 16:22:39 +0900

    謎コンフリ回避...

--- updater/ui.py
+++ updater/ui.py
@@ -10,16 +10,7 @@ class UpdaterWindow:
         self.root.resizable(False, False)
         self.root.attributes('-topmost', True)
 
-        # Set custom background and text color
-        self.root.configure(bg="#313842")
-        
-        self.label = tk.Label(
-            self.root,
-            text="Initializing...",
-            font=("Arial", 16),
-            bg="#313842",
-            fg="#eeeeee"
-        )
+        self.label = tk.Label(self.root, text="Initializing...", font=("Arial", 16))
         self.label.pack(expand=True)
 
     def set_status(self, text):
@@ -31,9 +22,3 @@ class UpdaterWindow:
 
     def close(self):
         self.root.destroy()
-
-if __name__ == "__main__":
-    window = UpdaterWindow()
-    window.run_in_thread()
-    window.set_status("Updater is running...")
-    window.root.mainloop()
